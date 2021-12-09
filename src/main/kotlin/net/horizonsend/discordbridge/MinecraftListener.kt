ebbe21b5a4a537ed2aa09fcff98e6738995ebbaa
Peter-Crawley <peterzcrawley@gmail.com>
2021-12-09 16:56:23 +0000

    Remove unused imports

--- src/main/kotlin/net/horizonsend/discordbridge/MinecraftListener.kt
+++ src/main/kotlin/net/horizonsend/discordbridge/MinecraftListener.kt
@@ -1,17 +1,12 @@
 package net.horizonsend.discordbridge
 
-import com.destroystokyo.paper.event.server.ServerTickEndEvent
 import io.papermc.paper.event.player.AsyncChatEvent
-import net.dv8tion.jda.api.MessageBuilder
 import net.dv8tion.jda.api.entities.Activity
 import net.ess3.api.events.MuteStatusChangeEvent
 import net.horizonsend.discordbridge.DiscordBridge.Companion.discord
 import net.horizonsend.discordbridge.DiscordBridge.Companion.globalChannel
-import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer.plainText
-import org.apache.logging.log4j.message.Message
 import org.bukkit.Bukkit
-import org.bukkit.Bukkit.getBannedPlayers
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
