bf126aae9cf3a6565ec3417bbbed96b2df74182d
Peter-Crawley <peterzcrawley@gmail.com>
2021-12-09 17:30:39 +0000

    Some event things

--- src/main/kotlin/net/horizonsend/discordbridge/MinecraftListener.kt
+++ src/main/kotlin/net/horizonsend/discordbridge/MinecraftListener.kt
@@ -8,27 +8,34 @@ import net.horizonsend.discordbridge.DiscordBridge.Companion.globalChannel
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer.plainText
 import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 
 class MinecraftListener: Listener {
-	@EventHandler
+	@EventHandler(priority = EventPriority.MONITOR)
 	fun onPlayerJoin(event: PlayerJoinEvent) {
 		discord.presence.activity = Activity.playing(" with ${Bukkit.getOnlinePlayers().size} player${if (Bukkit.getOnlinePlayers().size == 1) "" else "s"}")
 	}
 
-	@EventHandler
+	@EventHandler(priority = EventPriority.MONITOR)
 	fun onPlayerLeave(event: PlayerQuitEvent) {
 		discord.presence.activity = Activity.playing(" with ${Bukkit.getOnlinePlayers().size - 1} player${if (Bukkit.getOnlinePlayers().size - 1 == 1) "" else "s"}")
 	}
 
-	@EventHandler
+	@EventHandler(priority = EventPriority.MONITOR)
 	fun onPlayerChat(event: AsyncChatEvent) {
 		discord.getTextChannelById(globalChannel)!!.sendMessage(plainText().serialize(event.message())).queue();
 	}
 
-	@EventHandler
+	@EventHandler(priority = EventPriority.MONITOR)
+	fun onPlayerDeath(event: PlayerDeathEvent) {
+
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
 	fun onPlayerMute(event: MuteStatusChangeEvent) {
 		// Player mute event
 	}
